5a69da6e709be3dcecc8b738aa7679739509af74
FaceStudio <whospeaker@gmail.com>
2019-10-17 10:23:18 +0800

    增加广播传递surface

--- app/src/main/java/com/ott/playtest/MainActivity.java
+++ app/src/main/java/com/ott/playtest/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Surface;
 import android.view.TextureView;
+import android.view.View;
+import android.widget.Button;
 
 import com.ott.playserver.IPlayAidlInterface;
 
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
 
     private TextureView textureView;
     private Surface surface;
+
+    private Button play_aidl,play_broadcast;
 
     private IPlayAidlInterface iPlayAidlInterface;
 
@@ -28,7 +32,7 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        textureView = (TextureView) findViewById(R.id.texture);
+        textureView = findViewById(R.id.texture);
         textureView.setSurfaceTextureListener(this);//设置监听函数 重写4个方法
 
         Intent intent = new Intent();
@@ -39,19 +43,43 @@ public class MainActivity extends AppCompatActivity implements TextureView.Surfa
         bindService(intent, new ConnectCallBack(), Context.BIND_AUTO_CREATE);
 
 
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                if (iPlayAidlInterface != null) {
-                    try {   //此处传递真正的参数
-                        iPlayAidlInterface.play(surface);
-                    } catch (RemoteException e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        }, 1000); //延时1s执行
+        play_aidl = findViewById(R.id.play_aidl);
+        play_broadcast = findViewById(R.id.play_broadcast);
 
+        play_aidl.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if (iPlayAidlInterface != null) {
+                            try {   //此处传递真正的参数
+                                iPlayAidlInterface.play(surface);
+                            } catch (RemoteException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                    }
+                }, 500); //延时1s执行
+            }
+        });
+
+        play_broadcast.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent mIntent = new Intent("com.inspur.play");
+                        Bundle bundle = new Bundle();
+                        bundle.putParcelable("surface", surface);// 序列化
+                        mIntent.putExtras(bundle);// 发送数据
+                        //发送广播
+                        sendBroadcast(mIntent);
+                    }
+                }, 500);
+            }
+        });
 
     }
 
--- playserver/src/main/java/com/ott/playserver/MainActivity.java
+++ playserver/src/main/java/com/ott/playserver/MainActivity.java
@@ -1,13 +1,66 @@
 package com.ott.playserver;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    String url_hls = "https://zhstatic.zhihu.com/cfe/griffith/zhihu2018_hd.mp4";
+
+    private MediaPlayer mMediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        IntentFilter myIntentFilter = new IntentFilter();
+        myIntentFilter.addAction("com.inspur.play");
+        //注册广播
+        registerReceiver(mBroadcastReceiver, myIntentFilter);
+
     }
+
+    private BroadcastReceiver mBroadcastReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            if (action.equals("com.inspur.play")) {
+
+                Surface surface = intent.getParcelableExtra("surface");
+                play(surface);
+
+            }
+        }
+
+    };
+
+
+    private void play(Surface surface) {
+        try {
+            mMediaPlayer = new MediaPlayer();
+            mMediaPlayer.setDataSource(getApplicationContext(), Uri.parse(url_hls));
+            mMediaPlayer.setSurface(surface);
+            mMediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mMediaPlayer.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                @Override
+                public void onPrepared(MediaPlayer mp) {
+                    mMediaPlayer.start();
+                }
+            });
+            mMediaPlayer.prepare();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 }
